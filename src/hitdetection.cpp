2fef9dc5d52252b44c0a9b46c09560ae310b79bf
Roni Juppi <roju269@gmail.com>
2019-05-26 13:58:51 +0300

    Better documentation and cleanup

--- src/hitdetection.cpp
+++ src/hitdetection.cpp
@@ -32,54 +32,34 @@ hit(const World& world, const Ray& ray, f32 tMin, f32 tMax, HitInfo& info) {
         f32 discSqrt = sqrt(discriminant);
         f32 temp;
 
+        //
+        // Calculate the two possible results
+        //
         temp = (-b - discSqrt) / a;
-        bool mask1 = (temp < closestSoFar && temp > tMin);
+        u32 mask1 = (temp < closestSoFar && temp > tMin);
         HitInfo tempInfo1 = tempInfo;
         tempInfo1.t = temp;
         tempInfo1.point = pointOnRay(ray, temp);
         tempInfo1.normal = (tempInfo1.point - sphere->center) / sphere->radius;
 
         temp = (-b + discSqrt) / a;
-        bool mask2 = (temp < closestSoFar && temp > tMin);
+        u32 mask2 = (temp < closestSoFar && temp > tMin);
         HitInfo tempInfo2 = tempInfo;
         tempInfo2.t = temp;
         tempInfo2.point = pointOnRay(ray, temp);
         tempInfo2.normal = (tempInfo2.point - sphere->center) / sphere->radius;
 
-#if 1
-        tempInfo = (discriminant > 0)
-            ? (mask1
-                ? tempInfo1
-                : (mask2
-                    ? tempInfo2
-                    : tempInfo))
-            : tempInfo;
-#endif
+        // Pick the correct result, or none
+        //   if discriminant > 0:
+        //     Pick tempInfo1 if that is valid, otherwise tempInfo2,
+        //     otherwise the one we already had 
+        //   else
+        //     pick the one we already had
+        HitInfo temp1 = mask2 ? tempInfo2 : tempInfo;
+        HitInfo temp2 = mask1 ? tempInfo1 : temp1;
+        tempInfo = (discriminant > 0) ? temp2 : tempInfo;
 
         hit = mask1 || mask2;
-#if 0
-        f32 discriminant = b * b - a * c;
-        if (discriminant > 0) {
-            f32 discSqrt = sqrt(discriminant);
-
-            f32 temp = (-b - discSqrt) / a;
-            if (temp < closestSoFar && temp > tMin) {
-                tempInfo.t = temp;
-                tempInfo.point = pointOnRay(ray, tempInfo.t);
-                tempInfo.normal = (tempInfo.point - sphere->center) / sphere->radius;
-                hit = true;
-                goto end;
-            }
-            temp = (-b + discSqrt) / a;
-            if (temp < closestSoFar && temp > tMin) {
-                tempInfo.t = temp;
-                tempInfo.point = pointOnRay(ray, tempInfo.t);
-                tempInfo.normal = (tempInfo.point - sphere->center) / sphere->radius;
-                hit = true;
-                goto end;
-            }
-        }
-#endif
         hitSomething = hit || hitSomething;
         closestSoFar = hit ? tempInfo.t : closestSoFar;
         info = hit ? tempInfo : info;
